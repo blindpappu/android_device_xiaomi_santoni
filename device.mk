ebc8c271863dcbc67dd211ea22222b186e9c355d
Asriel Dreemurr <asriel.danctnix@gmail.com>
2019-06-30 19:44:58 +0700

    ubuntu/bluetooth-touch-santoni.conf: bluetooth initialization on ubports
    Signed-off-by: Asriel Dreemurr <asriel.danctnix@gmail.com>

--- device.mk
+++ device.mk
@@ -281,8 +281,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
-# Ubuntu Touch Files
-##
+######################
+# Ubuntu Touch Files #
+######################
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ubuntu/70-android.rules:system/halium/lib/udev/rules.d/70-android.rules \
+    $(LOCAL_PATH)/ubuntu/bluetooth-touch-santoni.conf:system/halium/etc/init/bluetooth-touch-android.conf \
     $(LOCAL_PATH)/ubuntu/timekeeper.conf:system/halium/etc/init/timekeeper.conf
