5be14213d85c60be3869674632ae58a714ae9c84
blindpappu <57904642+blindpappu@users.noreply.github.com>
2019-11-18 19:19:05 +0530

    Update init_setup_first_boot.sh

--- ubuntu/init_setup_first_boot.sh
+++ ubuntu/init_setup_first_boot.sh
@@ -5,11 +5,12 @@ mount -o remount rw /
 
 
 ## This is run if /lib/firmware doesn't exist
-if [ ! -d /lib/firmware ] ; then
+if [ ! -d /lib/firmware ] ; 
+then
 	mkdir -p /lib/firmware || true
 	
 	# Symlink the firmware files and reboot to take effect
 	ln -s /firmware/image/* /lib/firmware/
 	sleep 2
 	reboot
-fi
+if
